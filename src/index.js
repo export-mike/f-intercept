62983ec15aa32329ef7682f3b4848a2d536ff849
Mike James <m.l.james@me.com>
2016-04-06 21:24:46 +0100

    initial f library

---
+++ src/index.js
@@ -0,0 +1,47 @@
+// import eFetch from '@export-mike/efetch';
+import curry from 'curry';
+import url from 'url';
+const subscribers = {};
+const UNAUTH = 401;
+
+const intercept = (base, path, options, response) => {
+  // interception
+  if (response.status && response.status === UNAUTH) {
+    const subscribersForBase = subscribers[base];
+    if (subscribersForBase) {
+      try {
+        subscribersForBase.forEach(cb => cb(response.clone()));
+      } catch (e) {
+        // we dont want this error going up the parent chain in userland as its a different chain
+        if (process.NODE_ENV !== 'production') {
+          console.error('Unhandled Exception in onUnauth subscriber', e); // eslint-disable-line
+        }
+      }
+    }
+  }
+  // continue promise chain
+  return response;
+};
+
+const f = (base, path, options) => {
+  return eFetch(url.resolve(base, path), options)
+  .then(curry(intercept)(base, path, options));
+};
+
+export const onUnauth = (base, cb) => {
+  let subscribersForBase = subscribers[base];
+
+  if (!subscribersForBase) {
+    subscribers[base] = [cb];
+  }
+
+  subscribersForBase = subscribers[base];
+
+  subscribersForBase.push(cb);
+
+  return () => {
+    subscribersForBase = subscribersForBase.filter(s => s !== cb);
+  };
+};
+
+export default curry(f);
